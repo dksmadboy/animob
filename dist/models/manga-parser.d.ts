aff010f6398fd4461da19dbfd03d123218b4badf
animob <dkshacker77@gmail.com>
2026-03-06 20:42:09 +0530

    Add files via upload

---
+++ dist/models/manga-parser.d.ts
@@ -0,0 +1,17 @@
+import BaseParser from './base-parser';
+import { IMangaInfo, IMangaChapterPage } from './types';
+declare abstract class MangaParser extends BaseParser {
+    /**
+     * takes manga id
+     *
+     * returns manga info with chapters
+     */
+    abstract fetchMangaInfo(mangaId: string, ...args: any): Promise<IMangaInfo>;
+    /**
+     * takes chapter id
+     *
+     * returns chapter (image links)
+     */
+    abstract fetchChapterPages(chapterId: string, ...args: any): Promise<IMangaChapterPage[]>;
+}
+export default MangaParser;
